0640c12cdcf41c3ce0764f2ea53d2967620a7ccb
whw3 <whw3@users.noreply.github.com>
2017-08-06 06:10:38 -0500

    shellcheck audit

--- build.sh
+++ build.sh
@@ -7,17 +7,18 @@ if  [[ "$(which jq)" = "" ]]; then
 fi
 BASEDIR="${BASEDIR:-/srv/docker/buildpack-deps/}"
 REPO=buildpack-deps
+echo "BASEDIR:$BASEDIR"
 # Always remove and refresh
-[[ -d  $BASEDIR/$REPO ]] &&  \
-  rm -rf $BASEDIR/$REPO
+[[ -d  "$BASEDIR/$REPO" ]] &&  \
+  rm -rf "$BASEDIR/$REPO"
 
-cd $BASEDIR
+cd "$BASEDIR"
 git clone https://github.com/docker-library/buildpack-deps.git
 ### patch ###
 cd $REPO/jessie/
 sed -i 's_FROM debian:jessie_FROM whw3/rpi_' ./curl/Dockerfile
 sed -i 's/^RUN apt-get update \&\& apt-get install/RUN apt-get update \&\& apt-get upgrade \&\& apt-get install/' ./curl/Dockerfile
-find . -name Dockerfile| xargs sed -i 's_FROM buildpack-deps:jessie_FROM whw3/buildpack-deps:rpi_'
+find . -name Dockerfile -exec sed -i 's_FROM buildpack-deps:jessie_FROM whw3/buildpack-deps:rpi_' {} +
 ### build ###
 cd ./curl
 docker build -t whw3/buildpack-deps:rpi-curl .
@@ -27,14 +28,14 @@ cd ..
 docker build -t whw3/buildpack-deps:rpi .
 
 ### Its quicker to ADD s6-overlay to the cached images we just built than to re-baseline from whw3/rpi-s6
-cd $BASEDIR
+cd "$BASEDIR"
 
 wget -qO s6-tags.json https://api.github.com/repos/just-containers/s6-overlay/tags
 eval "$(jq -r '.[0] | @sh "S6_VERSION=\(.name)"' s6-tags.json )"
 rm s6-tags.json
 
 [[ ! -f s6-overlay-$S6_VERSION-armhf.tar.gz ]] && \
-    wget -O s6-overlay-$S6_VERSION-armhf.tar.gz https://github.com/just-containers/s6-overlay/releases/download/$S6_VERSION/s6-overlay-armhf.tar.gz
+    wget -O s6-overlay-"$S6_VERSION"-armhf.tar.gz https://github.com/just-containers/s6-overlay/releases/download/"$S6_VERSION"/s6-overlay-armhf.tar.gz
 
 declare -a tags=("-curl" "-scm" "")
 for tag in "${tags[@]}"
